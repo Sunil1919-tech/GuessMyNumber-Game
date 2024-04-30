c41a2a6ef7d0f750eb3e68b69a941507343b227f
Sunil Gollapalli <82632456+Sunil1919-tech@users.noreply.github.com>
2024-04-30 14:41:52 +0530

    Added no. of tries taken to be visible on screen

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const btn = document.getElementsByClassName("btn")[0];
 
 // logic to get the random num btw 1-10
 const randNum = parseInt(Math.random() * 10 + 1);
-console.log(randNum);
+// console.log(randNum);
 
 // accessing the form element
 const form = document.getElementsByClassName("border")[0];
@@ -13,18 +13,28 @@ const form = document.getElementsByClassName("border")[0];
 // accessing h1 = notice element
 const notice = document.getElementById("notice");
 
+//access score class
+const score = document.getElementsByClassName("score")[0];
+
+var count = 0;
+
 btn.addEventListener("click", function (event) {
   event.preventDefault();
   const num = document.getElementById("num").value;
   if (randNum > num) {
     notice.innerText = "Try a Bigger Number ⬆️";
     form.style.borderColor = "red";
+    count++;
+    score.innerText = count;
   } else if (randNum == num) {
     notice.innerText = "Congrats㊗️㊗️";
     form.style.borderColor = "green";
+    score.innerText = count;
   } else {
     notice.innerText = "Try a Smaller Number ⬇️";
     form.style.borderColor = "red";
+    count++;
+    score.innerText = count;
   }
 
   form.reset();
